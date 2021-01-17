1cf707775bad76a55dbb5805d92ecdaa079cdd1c
Takuhei Mizutani <takuheim1011@gmail.com>
2021-01-17 21:11:57 +0900

    gcp key;

---
+++ test.js
@@ -0,0 +1,24 @@
+const {Translate} = require('@google-cloud/translate').v2;
+
+// Creates a client
+const translate = new Translate();
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+const text = 'Hello there'
+const target = 'ja';
+
+async function translateText() {
+  // Translates the text into the target language. "text" can be a string for
+  // translating a single piece of text, or an array of strings for translating
+  // multiple texts.
+  let [translations] = await translate.translate(text, target);
+  translations = Array.isArray(translations) ? translations : [translations];
+  console.log('Translations:');
+  translations.forEach((translation, i) => {
+    console.log(`${text[i]} => (${target}) ${translation}`);
+  });
+}
+
+translateText();
